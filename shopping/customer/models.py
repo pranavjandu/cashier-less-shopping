c80e9aa539f81d88e7984ef61a42c2e211581ec1
Pranav Jandu <pranav.jandu@gmail.com>
2020-12-21 03:24:16 +0530

    temporary scanner app

--- shopping/customer/models.py
+++ shopping/customer/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Customer(models.Model):
+    readonly_fields = ('id',)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id) +". "+ self.user.username
+
+class Product(models.Model):
+    readonly_fields = ('id',)
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.ImageField(null=True, blank = True)
+
+    def __str__(self):
+        return str(self.id) +". "+ self.title
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.customer.save()
--- shopping/customer/views.py
+++ shopping/customer/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .models import Customer
 
-# Create your views here.
+
+
+def mainpage(request):
+    '''
+    For logging users in
+    '''
+    if request.method == "POST":
+        type = request.POST.get('type')
+        if type=="login":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user=authenticate(request,username=username,password=password)  #django default authentication
+            if user is not None:
+                login(request,user)
+                return redirect('customerhomepage')
+            else: 
+                messages.error(request,"Wrong Credentials! Please Try again")
+        elif type=="register":
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            try:   #creating Customer object
+                user=User.objects.create_user(username=username,password=password,email=email)
+                user.save()
+                messages.success(request," Signup successfull ")
+                return redirect('customerhomepage')
+            except:
+                messages.error(request," Error occured. Please Try again!")
+                return redirect('loginpage')
+    return render(request, 'login.html')   
+
+
+def dashboard(request):
+    return render(request, 'index.html')   
